67baff82244c6af402e67a1dee2f5ed3ea507b24
Jordan M <jmcclusky17@gmail.com>
2018-03-21 14:43:33 -0500

    Finnished "Add Menu" in Part 3.

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CheeseMVC.Data;
 using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CheeseMVC.Controllers
@@ -21,6 +22,27 @@ namespace CheeseMVC.Controllers
         {
             List<Menu> menus = context.Menus.ToList();
             return View();
+        }
+
+        public IActionResult Add()
+        {
+            AddMenuViewModel addMenuViewModel = new AddMenuViewModel();
+            return View(addMenuViewModel);
+        }
+
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name
+                };
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            }
+            return View(addMenuViewModel);
         }
 
     }
