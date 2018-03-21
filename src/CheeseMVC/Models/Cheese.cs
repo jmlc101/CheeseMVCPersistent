79e3d4e67a24635c87e027267b3bcb9b4a51e852
Jordan M <jmcclusky17@gmail.com>
2018-03-21 13:46:31 -0500

    Completed Up to "Controller and Views" within Part 3"

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -1,4 +1,6 @@
-﻿namespace CheeseMVC.Models
+﻿using System.Collections.Generic;
+
+namespace CheeseMVC.Models
 {
     public class Cheese
     {
@@ -11,6 +13,8 @@
         // TODO - added this from video...
         public int CategoryID { get; set; }
         public CheeseCategory Category { get; set; }
-        
+
+        public IList<CheeseMenu> CheeseMenus { get; set; } = new List<CheeseMenu>();
+
     }
 }
---
+++ src/CheeseMVC/Models/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class Menu
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        // TODO - Notice the get's and set's added below? needed?
+        public IList<CheeseMenu> CheeseMenus { get; set; } = new List<CheeseMenu>();
+
+    }
+}
